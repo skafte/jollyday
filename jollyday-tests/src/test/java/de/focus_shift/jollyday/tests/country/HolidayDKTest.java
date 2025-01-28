1c77386efc8c085358ca29db9ea7a685a44311b8
Klaus Skafte <klaus@lookshy.dk>
2025-01-28 11:43:08 +0100

    Update HolidayDKTest.java
    update test

--- jollyday-tests/src/test/java/de/focus_shift/jollyday/tests/country/HolidayDKTest.java
+++ jollyday-tests/src/test/java/de/focus_shift/jollyday/tests/country/HolidayDKTest.java
@@ -8,6 +8,7 @@ import static de.focus_shift.jollyday.core.HolidayCalendar.DENMARK;
 import static de.focus_shift.jollyday.tests.CalendarCheckerApi.assertFor;
 import static java.time.Month.DECEMBER;
 import static java.time.Month.JANUARY;
+import static java.time.Month.JUNE;
 
 class HolidayDKTest {
 
@@ -15,19 +16,23 @@ class HolidayDKTest {
   void ensuresHolidays() {
     assertFor(DENMARK)
       .hasFixedHoliday("NEW_YEAR", JANUARY, 1).and()
+      .hasFixedHoliday("CHRISTMAS_EVE", DECEMBER, 24).and()
       .hasFixedHoliday("CHRISTMAS", DECEMBER, 25).and()
-      .hasFixedHoliday("STEPHENS", DECEMBER, 26).and()
-      .hasChristianHoliday("EASTER").and()
+      .hasFixedHoliday("BOXING_DAY", DECEMBER, 26).and()
+      .hasFixedHoliday("NEW_YEARS_EVE", DECEMBER, 31).and()
+      .hasFixedHoliday("CONSTITUTION_DAY", JUNE, 5).and()
       .hasChristianHoliday("MAUNDY_THURSDAY").and()
       .hasChristianHoliday("GOOD_FRIDAY").and()
       .hasChristianHoliday("EASTER_MONDAY").and()
+      .hasChristianHoliday("MAUNDY_THURSDAY").and()
       .hasChristianHoliday("GENERAL_PRAYER_DAY")
        .between(Year.of(1990), Year.of(2023))
        .notBetween(Year.of(2024), Year.of(2500))
       .and()
       .hasChristianHoliday("ASCENSION_DAY").and()
       .hasChristianHoliday("WHIT_MONDAY").and()
-      .hasChristianHoliday("PENTECOST")
+      .hasChristianHoliday("PENTECOST").and()
+      .hasChristianHoliday("PENTECOST_MONDAY")
       .check();
   }
 }
